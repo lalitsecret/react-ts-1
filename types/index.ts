0de7886917a68607b0920b30c3ea92bdebc7b333
lalitsecret <lalitsecret@gmail.com>
2021-02-28 08:50:12 +0530

    1 file added

---
+++ types/index.ts
@@ -0,0 +1,23 @@
+export type products
+{
+  id:number;
+  title:string;
+  description:string;
+  old:number;
+  new:number;
+  rating:number;
+  discount:number;
+  tags:string;
+  category:string;
+  image:string;
+}
+export type tags
+{
+  t:string;
+  c:number;
+}
+export type cart
+{
+  
+}
+
