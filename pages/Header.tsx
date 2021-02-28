ac7e05dbf814b6b309d311f8f54f16324f9ac7fb
lalitsecret <lalitsecret@gmail.com>
2021-02-28 09:27:14 +0530

    1 file modified

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class Header extends React.Component
 {
   render()
   {
-    return <header>Header Works</header>
+    return ( <header> 
+            <div className="item">
+              <h1>ecommerce lalit </h1>
+            <div>
+
+      <div className="item">
+        <Link to="/">Home</Link>
+        <Link to="/Login">Login</Link>
+        <Link to="/Signup">Signup</Link>
+        <Link to="/Cart">Cart</Link>
+        <Link to="/Orders">Orders</Link>
+        <Link to="/Profile">Profile</Link>
+        <Link to="/Checkout">Checkout</Link>
+        <Link to="/Logout">Logout</Link>
+      <div>
+    </header> );
   }
 }
 export default Header;
